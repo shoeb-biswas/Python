d84dd9ca29563eaceb02b9d93f08fb92b04392a0
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-06 21:52:58 +0600

    py 1012
    Make a program that reads three floating point values: A, B and C. Then, calculate and show:
    a) the area of the rectangled triangle that has base A and height C.
    b) the area of the radius's circle C. (pi = 3.14159)
    c) the area of the trapezium which has A and B by base, and C by height.
    d) the area of ​​the square that has side B.
    e) the area of the rectangle that has sides A and B.

---
+++ Area.py
@@ -0,0 +1,14 @@
+a,b,c=input().split()
+a=float(a)
+b=float(b)
+c=float(c)
+v=(a*c)/2
+w=3.14159*c**2
+x=((a+b)*c)/2
+y=b**2
+z=a*b
+print(f"TRIANGULO: {v:.3f}")
+print(f"CIRCULO: {w:.3f}")
+print(f"TRAPEZIO: {x:.3f}")
+print(f"QUADRADO: {y:.3f}")
+print(f"RETANGULO: {z:.3f}")
