808d6ad552aa18b672acc053110157d7fe21e88b
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-09 11:13:11 +0600

    Practice Jupyter 5.py
    Create a 1D, 2D, and 3D NumPy array and print their dimensions and shapes.

---
+++ Jupyter 4.py
@@ -0,0 +1,13 @@
+import numpy as np
+dimen1 = np.array([2,7,4,3,6])
+dimen2 = np.array([[8,7,6],[8,0,3]])
+dimen3 = np.array([[[5,9,3],[7,4,1]], [[8,3,6],[8,4,3]]])
+
+print("Shape:",dimen1.shape)
+print("Dimensions:",dimen1.ndim)
+
+print("Shape:",dimen2.shape)
+print("Dimensions:",dimen2.ndim)
+
+print("Shape:",dimen3.shape)
+print("Dimensions:",dimen3.ndim)
