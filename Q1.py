1e4b41e19e17584cb11391fa373de4d53f62d739
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-10 09:34:19 +0600

    Mid Eaxam 01 Q1.py
    Q1. Create NumPy arrays from the ages and salaries data generated above. Print both arrays and display their dtype, ndim, shape, and size.

---
+++ Q1.py
@@ -0,0 +1,17 @@
+import numpy as np
+age_array = np.array(ages)
+sal_array = np.array(salaries)
+
+print("Age:\n",age_array)
+
+print("\nData Type:\n",age_array.dtype)
+print("Dimension:\n",age_array.ndim)
+print("Shape:\n",age_array.shape)
+print("Size:\n",age_array.size)
+
+print("\nSalary:\n",sal_array)
+
+print("\nData Type:\n",sal_array.dtype)
+print("Dimension:\n",sal_array.ndim)
+print("Shape:\n",sal_array.shape)
+print("Size:\n",sal_array.size)
