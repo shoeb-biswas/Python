cf9150dcaec1081340581c612d3c6e46e2d020e9
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-10 09:35:09 +0600

    Mid Exam 01 Q2.py
    Q2. Using the salaries array, find and print the highest and lowest salary values. Also, calculate and print the average salary and age using NumPy functions.

---
+++ Q2.py
@@ -0,0 +1,10 @@
+import numpy as np
+Highest_value = max(sal_array)
+Lowest_value = min(sal_array)
+Avg_sal = sum(sal_array) / len(sal_array)
+Avg_age = sum(age_array)/len(age_array)
+
+print("Highest salary value:",Highest_value)
+print("Lowest salary value:",Lowest_value)
+print("Average salary:",Avg_sal)
+print("Average age",Avg_age)
