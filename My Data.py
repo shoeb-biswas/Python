3f0c576440ddaee955f03e24f98318d016a2576d
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-12 08:39:18 +0600

    Practice My Data.py

---
+++ My Data.py
@@ -0,0 +1,7 @@
+my_data = [
+{'Name': 'max_mayfield', 'Age':21, 'City': 'undefiened'},
+{'Name': 'Erica_Cincliar', 'Age':12, 'City': 'Howkin'},
+{'Name': 'Lucas_Cinclair', 'Age':21},
+]
+data_df = pd.DataFrame(my_data)
+data_df
