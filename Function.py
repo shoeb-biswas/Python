60bed7324c36bfa03b487c75b02852b7c7491e36
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-15 18:47:30 +0600

    Practice Function.py

---
+++ Function.py
@@ -0,0 +1,10 @@
+def greeting():
+  print("Welcome to this conceptual session - 1 ")
+
+greeting()
+
+# Positional Parameters
+def greeting(name2, name1):
+  print("Welcome to this conceptual session - 1 ", name1)
+
+greeting("Rakib", "Yasin") #
