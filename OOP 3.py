b54a554ebe1e913fd2777386dbfa540826973536
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-15 18:41:57 +0600

    Practice OOP 3.py

---
+++ OOP 3.py
@@ -0,0 +1,10 @@
+apple = Phone("3000mah", "12MP", "Iphone17")
+apple.category = "Technology"
+blueberry = Phone("5000mah","200MP","Ibot")
+vivo = Phone("7000mah","20MP","Vivo21")
+
+print(apple.camera)
+print(blueberry.battry)
+print(vivo.model)
+apple.capture(10)
+print(apple.battry_level)
