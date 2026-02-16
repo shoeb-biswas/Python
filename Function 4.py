9300590cbf9f0e40677c4b315bdfc5c60202ce83
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-16 08:18:10 +0600

    Practice Function 4.py

---
+++ Function 4.py
@@ -0,0 +1,11 @@
+# Dictionary Return
+def get_student():
+    student = {
+        "name": "Rakib",
+        "age": 21,
+        "city": "Dhaka"
+    }
+    return student
+
+info = get_student()
+print(info['name'])
