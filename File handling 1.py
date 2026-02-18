88061c91609fc0c40c21910a1570a1d8e68172e3
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-18 08:17:21 +0600

    Create File handling 1.py
    Create a text file named emails.txt containing multiple email addresses, some in uppercase and some repeated. Write a Python program to clean the data by converting all emails to lowercase, removing duplicates, sorting them alphabetically, and saving the cleaned list to a new file named unique_emails.txt.

---
+++ File handling 1.py
@@ -0,0 +1,5 @@
+with open("./sample_data/emails.txt","w") as file:
+  file.write("Hello@gmail.com\n")
+  file.write("Hello@gmail.com\n")
+  file.write("sillymen@gmail.com\n")
+  file.write("Aillymen@gmail.com")
