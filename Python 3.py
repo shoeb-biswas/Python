b901abff53ce8d3006517dbb69c17937b1bfd732
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-20 11:16:11 +0600

    Practice Python 3.py

---
+++ Python 3.py
@@ -0,0 +1,12 @@
+string = "Welcome to phitron ML course, phitron"
+string1 = string.replace("ML","AI/ML")
+print(string1)
+
+prompt = "What is your name ?"
+token = prompt.split()
+print(token)
+
+prompt = "What is your name ?"
+token = prompt.split()
+token = "-".join(token)
+print(token)
