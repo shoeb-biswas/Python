1b0928fe3ba9826469b520e8d453578417aa8c55
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-21 13:47:07 +0600

    Practice Python 18.py

---
+++ Python 18.py
@@ -0,0 +1,14 @@
+#Size
+print(arr1.size)
+print(arr2.size)
+print(arr3.size)
+
+#Diamention
+print(arr1.ndim)
+print(arr2.ndim)
+print(arr3.ndim)
+
+#Shape
+print(arr1.shape)
+print(arr2.shape)
+print(arr3.shape)
