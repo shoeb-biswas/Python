93b53c1f8915359e4e1126437a974b5f2262a92f
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-03-04 03:01:05 +0600

    Scatter plot 3

---
+++ Scatter plot 3.py
@@ -0,0 +1,5 @@
+plt.xlabel('Chilling Hours')
+plt.ylabel('IQ Score')
+plt.title('Relation between Chilling Hours and IQ Score')
+
+plt.scatter(students['Chilling_Hours'],students['IQ_Score'],color='red')
