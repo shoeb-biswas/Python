554ede3cc1d20c868988f9db6734fea96008f018
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-05-14 16:44:33 +0600

    Info

---
+++ Info.py
@@ -0,0 +1,10 @@
+# Basic info about row, columns and data types
+print("Shape: ", df.shape)
+
+print("info: ")
+df.info()
+
+# Quick Descriptive Statistics for Numerical Column
+df.describe()
+
+df.describe().T
