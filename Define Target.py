de6771c447fc8dccc6f436fae419eb4e5dae2784
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-05-14 16:44:58 +0600

    Define Target

---
+++ Define Target.py
@@ -0,0 +1,7 @@
+# Define Target and Feature Types
+target_col = "HeartDisease"
+numeric_cols = ["Age", "RestingBP", "Cholesterol", "FastingBS","MaxHR", "Oldpeak" ]
+Categorical_cols = ["Sex", "ChestPainType", "RestingECG", "ExerciseAngina", "ST_Slope"]
+print(target_col)
+print(numeric_cols)
+print(Categorical_cols)
